065cdf0c89a455b9686f2ae876f135cd5ef83b42
unknown <jess@ETRACSSERVER.TRACS.GOV>
2019-04-15 16:04:23 +0800

    added bin, lib

---
+++ bin/run.bat
@@ -0,0 +1,18 @@
+@echo off
+rem Setup the java command
+rem set JAVA_HOME=C:\Users\ramesesinc\Apps\jdk7u80-x64
+set JAVA=java
+if not "%JAVA_HOME%" == "" set JAVA=%JAVA_HOME%\bin\java
+
+echo %JAVA_HOME%
+
+rem This will be the run directory
+set RUN_DIR=%cd%
+rem Move up...
+cd ..
+rem This will be the base directory
+set BASE_DIR=%cd%
+
+set JAVA_OPT="-Xmx1500m -Dosiris.run.dir=%RUN_DIR% -Dosiris.base.dir=%BASE_DIR%"
+
+"%JAVA%" "%JAVA_OPT%" -cp lib/*;. com.rameses.main.bootloader.MainBootLoader
